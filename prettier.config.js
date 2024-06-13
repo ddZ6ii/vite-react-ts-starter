82335dd6a91504d90feeba0774603453cc674ae4
Denis Dezest <denis.dezest@gmail.com>
2024-06-13 08:22:20 +0200

    📦 Add prettier, custom config and ignore files.

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  printWidth: 80,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
